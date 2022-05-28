852aedf2280f7bf469d1becd86c2daf52887d9fe
Renan Pereira <renanmol87@gmail.com>
2022-05-28 19:06:02 -0300

    feat: init project

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  endOfLine: 'lf',
+  arrowParens: 'avoid'
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <h1>Hello world!</h1>
+}
+
+export default Home
