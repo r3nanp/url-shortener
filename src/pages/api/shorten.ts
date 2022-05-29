6ee152d2e5a7c8f0d1fb45e4ac5f3af48d4e0e5b
Renan Pereira <renanmol87@gmail.com>
2022-05-28 22:46:51 -0300

    feat(api): create shorten endpoint

--- src/pages/api/shorten.ts
+++ src/pages/api/shorten.ts
@@ -2,12 +2,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  url: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const url = req.body.url
+
+  console.log(url)
+
+  res.status(200).json({ url })
 }
