6c210560f41f610ded9d8faf4b7a3c109587ad1f
Renan Pereira <renanmol87@gmail.com>
2022-05-28 22:47:48 -0300

    feat: add constants and create example for env

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string
+export const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL as string
+export const UPSTASH_REDIS_REST_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN as string
