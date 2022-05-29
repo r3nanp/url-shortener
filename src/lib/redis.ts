8d4df1624307734ad8a98046667c1986a1f4877a
Renan Pereira <renanmol87@gmail.com>
2022-05-29 01:20:37 -0300

    feat(redis): create function to get url value inside the redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,13 +4,21 @@ import { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } from '@/constants'
 
 const redis = new Redis({
   url: UPSTASH_REDIS_REST_URL,
-  token: UPSTASH_REDIS_REST_TOKEN,
+  token: UPSTASH_REDIS_REST_TOKEN
 })
 
 export async function setUrl(url: string) {
   const short = getShort()
 
-  await redis.set(`short/${short}`, url)
+  const result = await redis.set(`short/${short}`, url)
 
   return short
+}
+
+export async function getUrl(short: string): Promise<string> {
+  const data = await redis.get(`short/${short}`)
+
+  const result = data as string
+
+  return result
 }
