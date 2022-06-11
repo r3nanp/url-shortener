5899c5e49e23e1df5edf56a7847a213a2cac385d
Renan Pereira <renanmol87@gmail.com>
2022-06-11 00:33:45 -0300

    fix(lib): remove document object outside the DOM context

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ??
-    `http://localhost:${document.location.port}`
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000'
 })
